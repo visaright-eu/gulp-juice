c00511985982ef0779fb9a947c135433b222a46a
AntonPuko <stinger_anton@mail.ru>
2020-04-10 14:01:52 +0200

    pass juice instance to the plugin

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-var es = require('event-stream'),
-    gutil = require('gulp-util'),
-    juice = require('juice').juiceResources,
-    File = gutil.File,
-    PluginError = gutil.PluginError,
-    crypto = require('crypto');
+var es = require("event-stream"),
+  gutil = require("gulp-util"),
+  File = gutil.File,
+  PluginError = gutil.PluginError,
+  crypto = require("crypto");
 
-module.exports = function(options){
-  options = options || {};
-  return es.map(function(file, fn){
-      juice(file.contents.toString(), options, function(err, html){
-        if(err) return fn(err);
+module.exports = function (juiceInstance) {
+  return function (options) {
+    options = options || {};
+    return es.map(function (file, fn) {
+      juiceInstance.juiceResources(file.contents.toString(), options, function (
+        err,
+        html
+      ) {
+        if (err) return fn(err);
         file.contents = new Buffer(html);
         fn(null, file);
       });
     });
+  };
 };
